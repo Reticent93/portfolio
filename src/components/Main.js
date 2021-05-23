b9d2a3b9444b5c45e57839b72504a3520dd5262f
Greg <remph819@gmail.com>
2021-05-23 06:14:49 -0700

    working on more main.js styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Authentication from '../img/Authentication.jpg';
+import database from '../img/database.jpg';
 import game_of_life from '../img/game_of_life.jpg';
-import star_wars_api from '../img/star_wars_api.jpg';
+import Star_Wars from '../img/Star_Wars.jpg';
 import smurf from '../img/smurf.jpg';
 import space_photo from '../img/space_photo.jpg';
 import '../css/styles.css';
@@ -60,18 +60,19 @@ export default function Main() {
                     <a href="#contact">Contact Me</a>
                 </div>
             </div>
-            <div className="container flex-wrap" id="projects">
-                <div className="projects">
+            <div className="container" id="projects">
+
+                <div className="projects text-start">
                     <h1>Projects</h1>
                 </div>
 
-                <div className="row pictures-category">
+                <div className="row  pictures-category">
 
 
                     <figure className="col-4 auth">
                         <a href="authentication">
-                            <img src={Authentication} alt="authentication"
-                                 className='figure-img w-100 h-50 rounded-3'/>
+                            <img src={database} alt="authentication"
+                                 className='img-fluid figure-img rounded-3'/>
                         </a>
                         <p className='card-text text-white'>This is about backend authentication using Node.js and
                             Express.</p>
@@ -83,26 +84,30 @@ export default function Main() {
 
                     <figure className="col-4 game">
                         <a href="life">
-                            <img src={game_of_life} alt="Game of Life" className='figure-img w-100 h-50 rounded-3'/>
+                            <img src={game_of_life} alt="Game of Life" className='img-fluid figure-img rounded-3'/>
                         </a>
                         <p className='card-text text-white'>This is Conway's Game of Life. Click the image to see how it works.</p>
                         <a target="blank" href="https://github.com/Reticent93/CS-Build-Week-1"
                             className="btn btn-default">Code
                         </a>
                     </figure>
-                    <div className="w-100 d-none d-md-block"></div>
-                    <figure className="col-4 wars">
+
+                    <div className="w-100"></div>
+                    <div className="col-4 wars">
                         <a href="star-wars">
-                            <img src={star_wars_api} alt="Star Wars" className='figure-img w-100 h-50 rounded-3'/>
+                            <img src={Star_Wars} alt="Star Wars" className='img-fluid figure-img rounded-3'/>
                         </a>
                         <p className='card-text text-white'>This is a React app using the Star Wars API. Click the image to see how it works.</p>
                         <a target="blank" href="https://dazzling-blackwell-59fa62.netlify.app/"
                             className="btn btn-default">Code
                         </a>
-                    </figure>
+                    </div>
+
+
+
                     <figure className="col-4 smurfs">
                         <a href="smurf">
-                            <img src={smurf} alt="Smurf" className='figure-img w-100 h-50 rounded-3'/>
+                            <img src={smurf} alt="Smurf" className='img-fluid figure-img rounded-3'/>
                         </a>
                         <p className='card-text text-white'>This is an app using Redux. Click the image to see how it works.</p>
                         <a target="blank" href="https://github.com/Reticent93/Sprint-Challenge-State-Management-Smurfs"
@@ -110,10 +115,12 @@ export default function Main() {
                         </a>
                     </figure>
 
+
+
                 </div>
             </div>
-            <div className="skills" id="skills">
-                <h1>Skills</h1>
+            <div className="skills container " id="skills">
+                <h1 className="justify-content-end">Skills</h1>
                 <div className="skills-info">
                     <h3>Front End:</h3>
                     <p>CSS / Javascript / React / Redux / Bootstrap / LESS</p>
@@ -126,11 +133,11 @@ export default function Main() {
             <div className="about" id="about">
                 <h1>About</h1>
                 <div className="me-info">
-                    <p>
+                    <p className='text-white'>
                         Hello. My name is Greg Renfro and I am a Full Stack Web Developer. I enjoy building websites.
                     </p>
                 </div>
-                {/*<img src={space_photo} alt="Space"/>*/}
+                <img src={space_photo} alt="Space" className='img-fluid w-50 h-auto rounded-3' />
             </div>
         </div>
     );
