22ce7c48aade766a9efd4f76565710ec6733c3a5
Greg <remph819@gmail.com>
2021-05-24 11:48:29 -0700

    working on more main.js styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,9 +8,9 @@ export default function Footer() {
             <p className="fs-5 ms-5">
                 These are the places where I can be reached. Feel free to contact me.
             </p>
-            <a href="mailto:greg.renfro@gmail.com" className='ms-5 fs-5 text-white-50 text-decoration-none'><span>greg.renfro93@gmail.com</span></a>
-            <a href="https://www.linkedin.com/in/gregrenfro/" className="card-link ms-5 fs-5 text-white-50 text-decoration-none"><span>LinkedIn</span></a>
-            <a href="https://github.com/Reticent93" className="card-link ms-5 fs-5 text-white-50 text-decoration-none"><span>Github</span></a>
+            <a target='blank' href="mailto:greg.renfro@gmail.com" className='ms-5 fs-5 text-white-50 text-decoration-none'><span>greg.renfro93@gmail.com</span></a>
+            <a target='blank' href="https://www.linkedin.com/in/gregrenfro/" className="card-link ms-5 fs-5 text-white-50 text-decoration-none"><span>LinkedIn</span></a>
+            <a target='blank' href="https://github.com/Reticent93" className="card-link ms-5 fs-5 text-white-50 text-decoration-none"><span>Github</span></a>
         </div>
     );
 }
